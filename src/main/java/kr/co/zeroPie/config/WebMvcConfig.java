11d62ee813c23ae296da51e24620c67789d5cca7
NAIMJAE <skdlawo5985@gamil.com>
2024-05-23 03:05:14 +0900

    프론트랑 연결

--- src/main/java/kr/co/zeroPie/config/WebMvcConfig.java
+++ src/main/java/kr/co/zeroPie/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package kr.co.zeroPie.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,5 +11,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired
     private ResourceLoader resourceLoader;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 클라이언트 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
 
 }
