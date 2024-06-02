4c55e8b3ab5dce9722e41438e53bc81d7bccd19d
hyeji111544 <hyeji111544@gmail.com>
2024-06-02 21:52:34 +0900

    조직도 조회

--- src/main/java/kr/co/zeroPie/repository/custom/StfRepositoryCustom.java
+++ src/main/java/kr/co/zeroPie/repository/custom/StfRepositoryCustom.java
@@ -1,9 +1,9 @@
 package kr.co.zeroPie.repository.custom;
 
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import kr.co.zeroPie.dto.PageRequestDTO;
-import kr.co.zeroPie.dto.StfDTO;
 import kr.co.zeroPie.entity.Stf;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +14,8 @@ public interface StfRepositoryCustom {
 
     public Page<Stf> searchUserTypeAndKeyword(PageRequestDTO pageRequestDTO, Pageable pageable);
 
-    public List<StfDTO> stfRank ();
+    public List<Tuple> stfRank ();
+
+    public Tuple stfInfo (String stfNo);
 
 }
