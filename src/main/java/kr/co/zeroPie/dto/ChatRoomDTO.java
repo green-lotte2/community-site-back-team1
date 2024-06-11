c72463220b19bd43bd32aaa34c130142ab864336
oharam <oharam0413@gmail.com>
2024-06-11 17:53:39 +0900

    Modify.chatService

--- src/main/java/kr/co/zeroPie/dto/ChatRoomDTO.java
+++ src/main/java/kr/co/zeroPie/dto/ChatRoomDTO.java
@@ -13,34 +13,68 @@ import java.util.Set;
 @Slf4j
 @Getter
 @Setter
-@Builder
 public class ChatRoomDTO {
 
     private String roomId;
     private String name;
-    private Set<WebSocketSession> sessions;
+    private Set<WebSocketSession> sessions = new HashSet<>();
 
-    public ChatRoomDTO() {
-        this.sessions = new HashSet<>();
-    }
-
-    // 인자가 있는 생성자
-    public ChatRoomDTO(String roomId, String name, Set<WebSocketSession> sessions) {
+    @Builder
+    public ChatRoomDTO(String roomId, String name) {
         this.roomId = roomId;
         this.name = name;
-        this.sessions = sessions;
     }
 
     public void handleActions(WebSocketSession session, ChatMessageDTO chatMessage, ChatService chatService) {
+
+        log.info("여기 들어옴?......4 - session, chatMessage, chatService" + session+ "/"+ "/"+chatMessage+"/"+chatService);
+
         if (chatMessage.getType().equals(ChatMessageDTO.MessageType.ENTER)) {
             sessions.add(session);
             chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
         }
+
+        log.info("여기 들어옴?.......5");
         sendMessage(chatMessage, chatService);
     }
 
+    /*
+    구독자를 관리해야함 -> 채팅방에서 구독자들이 나가고(QUIT) 들어올때(ENTER)
+
+    private List<WebSocketSession> subscribers = new ArrayList<>();
+
+    public ChatRoom(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public void subscribe(WebSocketSession session) {
+        subscribers.add(session);
+    }
+
+    public void unsubscribe(WebSocketSession session) {
+        subscribers.remove(session);
+    }
+
+    public void sendMessageToSubscribers(Object message, ChatService chatService) {
+
+
+     */
+
     public <T> void sendMessage(T message, ChatService chatService) {
-        sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
+
+        log.info("여기 들어옴?.......6"+message+"/"+chatService);
+
+        log.info("sessions의 사이즈가 알고싶다 : "+sessions.size());
+        log.info("sessions의 값이 알고싶다 : "+sessions);
+        try{
+            //for(WebSocketSession ws :sessions){
+            //    if(ws.isOpen()){
+                    sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
+             //   }
+            //}
+        }catch (Exception e){
+            log.info(e.getMessage());
+        }
     }
 }
 
