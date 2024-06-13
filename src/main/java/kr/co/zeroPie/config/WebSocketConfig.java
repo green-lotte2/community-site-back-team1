fc1adb3eb7b33f0e11e7bf13019e2e36870e2ec0
NAIMJAE <153374452+NAIMJAE@users.noreply.github.com>
2024-06-13 18:22:34 +0900

    페이지 데이터 DB 저장

--- src/main/java/kr/co/zeroPie/config/WebSocketConfig.java
+++ src/main/java/kr/co/zeroPie/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
         // ws://localhost:8080/ws 으로 요청이 들어오면 websocket 통신을 진행한다.
         // setAllowedOrigins("*")는 모든 ip에서 접속 가능하도록 해줌
         registry.addHandler(webSocketHandler, "/ws").setAllowedOrigins("*");
-        registry.addHandler(webSocketHandler2, "/testaa").setAllowedOrigins("*");
+        registry.addHandler(webSocketHandler2, "/doc").setAllowedOrigins("*");
     }
 
 }
